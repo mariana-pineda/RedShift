973287680924365026fb98841554086881c2096a
Purgo AI <support@purgo.ai>
2024-09-02 10:47:11 -0500

    Commit message

---
+++ tpcds-marianaMig/table/catalog_page.Table.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE tpcds.catalog_page (
+  cp_catalog_page_sk INT NOT NULL,
+  cp_catalog_page_id STRING NOT NULL,
+  cp_start_date_sk INT,
+  cp_end_date_sk INT,
+  cp_department STRING,
+  cp_catalog_number INT,
+  cp_catalog_page_number INT,
+  cp_description STRING,
+  cp_type STRING,
+  PRIMARY KEY (cp_catalog_page_sk)
+) USING DELTA;
