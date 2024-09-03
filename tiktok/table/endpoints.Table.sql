dbb23c6bf698c62f2704bfd73b6646f1871a7d15
Purgo AI <support@purgo.ai>
2024-09-03 08:49:41 -0500

    Commit message

---
+++ tiktok/table/endpoints.Table.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE endpoints (
+	appname VARCHAR(256) NOT NULL, 
+	display_name VARCHAR(256), 
+	endpoints_url VARCHAR(1000), 
+	input VARCHAR(1000), 
+	output VARCHAR(1000), 
+	end_point VARCHAR(500), 
+	display_option VARCHAR(200)
+)
+
