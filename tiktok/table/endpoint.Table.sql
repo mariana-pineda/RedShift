386db3be96ec6bc093e46985c53e7c87f4c1cbe9
Purgo AI <support@purgo.ai>
2024-09-03 08:49:44 -0500

    Commit message

---
+++ tiktok/table/endpoint.Table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE endpoint (
+	app_name VARCHAR(256), 
+	process_name VARCHAR(256), 
+	sequence INTEGER, 
+	endpoint_name VARCHAR(256), 
+	endpoint_url VARCHAR(2000), 
+	input VARCHAR(2000), 
+	output VARCHAR(2000), 
+	display_option VARCHAR(256), 
+	display_name VARCHAR(258), 
+	type VARCHAR(258)
+)
+
