f5bdc4a3bb76b7afcce774777834b0a212e9ef47
Purgo AI <support@purgo.ai>
2024-09-03 08:49:46 -0500

    Commit message

---
+++ tiktok/table/config_table.Table.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE config_table (
+	app_name VARCHAR(256), 
+	process_name VARCHAR(256), 
+	history VARCHAR(256), 
+	start_date TIMESTAMPTZ, 
+	competitors VARCHAR(256), 
+	endpoint_name VARCHAR(258), 
+	days_threshold VARCHAR(258)
+)
+
