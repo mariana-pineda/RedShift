35f6fa006dcbdfc48427184e40ee6686e7295650
Purgo AI <support@purgo.ai>
2024-09-03 08:49:39 -0500

    Commit message

---
+++ tiktok/table/linkedin_post_metrics_tmp.Table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE linkedin_post_metrics_tmp (
+	post_text VARCHAR(255), 
+	sharecount INTEGER, 
+	sharementionscount INTEGER, 
+	engagement INTEGER, 
+	clickcount INTEGER, 
+	likecount INTEGER, 
+	impressioncount INTEGER, 
+	commentmentionscount INTEGER, 
+	commentcount INTEGER, 
+	time DATE
+)
+
