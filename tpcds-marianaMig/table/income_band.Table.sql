7ef6db5c8287611235e2cbbb13794d98732b99e9
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-09-04 15:11:20 -0500

    Delete tpcds-marianaMig directory

--- tpcds-marianaMig/table/income_band.Table.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE tpcds.income_band (
-  ib_income_band_sk INT NOT NULL, 
-  ib_lower_bound INT, 
-  ib_upper_bound INT, 
-  PRIMARY KEY (ib_income_band_sk) 
-) USING DELTA;
--- tpcds-marianaMig/table/reason.Table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tpcds.reason (
-    r_reason_sk INT NOT NULL, 
-    r_reason_id CHAR(16) NOT NULL, 
-    r_reason_desc CHAR(100), 
-    PRIMARY KEY (r_reason_sk)
-) USING delta
-DISTRIBUTE BY HASH (r_reason_sk)
