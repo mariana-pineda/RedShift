c0d1873ba6691ff61c7e0db57c37842a0ef42aa1
Purgo AI <support@purgo.ai>
2024-09-04 11:01:21 -0500

    Commit message

--- dbo-mig/table/Shippers.Table.sql
+++ dbo-mig/table/Shippers.Table.sql
@@ -1,20 +1,9 @@
 
 CREATE TABLE Shippers (
-    ShipperID INT NOT NULL,
-    CompanyName STRING NOT NULL, 
-    Phone STRING 
-)
-USING DELTA;
+    ShipperID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+    CompanyName STRING NOT NULL,
+    Phone STRING
+);
 
 ALTER TABLE Shippers
-ALTER COLUMN ShipperID SET NOT NULL;
-
-ALTER TABLE Shippers 
 ADD CONSTRAINT PK_Shippers PRIMARY KEY (ShipperID);
-
-CREATE SEQUENCE Shippers_ShipperID_seq START 1;
-
-CREATE OR REPLACE TRIGGER Shippers_ShipperID_trigger 
-BEFORE INSERT ON Shippers 
-FOR EACH ROW 
-SET new.ShipperID = nextval('Shippers_ShipperID_seq');
