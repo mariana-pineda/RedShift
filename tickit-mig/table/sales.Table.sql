8928bbff897fef3ac074ba71bbef77e3eb7d26c0
Purgo AI <support@purgo.ai>
2024-09-04 15:25:36 -0500

    Commit message

---
+++ tickit-mig/table/sales.Table.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE tickit.sales (
+  salesid INT, 
+  listid INT, 
+  sellerid INT, 
+  buyerid INT, 
+  eventid INT, 
+  dateid SMALLINT, 
+  qtysold SMALLINT, 
+  pricepaid DECIMAL(8, 2), 
+  commission DECIMAL(8, 2), 
+  saletime TIMESTAMP
+);
