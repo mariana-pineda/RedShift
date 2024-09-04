7f4fabf61f37f68c8f06e876c22b8845eb859f19
Purgo AI <support@purgo.ai>
2024-09-04 11:01:42 -0500

    Commit message

--- dbo-mig/view/OrderDetailsExtended.View.sql
+++ dbo-mig/view/OrderDetailsExtended.View.sql
@@ -1,11 +1,7 @@
 
 CREATE VIEW `Order Details Extended` AS
-SELECT `Order Details`.OrderID,
-       `Order Details`.ProductID,
-       Products.ProductName,
-       `Order Details`.UnitPrice,
-       `Order Details`.Quantity,
-       `Order Details`.Discount,
-       (CAST((`Order Details`.UnitPrice * Quantity * (1 - Discount) / 100) AS DECIMAL(19, 4)) * 100) AS ExtendedPrice
-FROM Products
-INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID
+SELECT `Order Details`.OrderID, `Order Details`.ProductID, Products.ProductName, 
+    `Order Details`.UnitPrice, `Order Details`.Quantity, `Order Details`.Discount, 
+    (CAST((`Order Details`.UnitPrice * Quantity * (1 - Discount)) AS DECIMAL(19,4))) AS ExtendedPrice
+FROM Products 
+INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID;
