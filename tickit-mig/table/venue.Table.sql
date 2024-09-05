d4b74ffb3aebbfd3e9c033281c4d0483cf134d64
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-09-04 19:54:50 -0500

    Delete tickit-mig directory

--- tickit-mig/table/venue.Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE tickit.venue (
-    venueid SMALLINT NOT NULL, 
-    venuename STRING, 
-    venuecity STRING, 
-    venuestate STRING, 
-    venueseats INT,
-    PRIMARY KEY (venueid)
-) USING DELTA;
