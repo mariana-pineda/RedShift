6165f0cdc624f8688e6f16e58f2a2030dd9d5e08
Purgo AI <support@purgo.ai>
2024-09-05 10:27:46 -0500

    Commit message

--- tickit-uat/table/venue.Table.sql
+++ tickit-uat/table/venue.Table.sql
@@ -1,9 +1,8 @@
-
 CREATE TABLE tickit.venue (
 	venueid SMALLINT NOT NULL, 
 	venuename VARCHAR(100), 
 	venuecity VARCHAR(30), 
 	venuestate CHAR(2), 
 	venueseats INTEGER
-) DISTSTYLE KEY DISTKEY (venueid) SORTKEY (venueid)
-
+) USING DELTA
+PRIMARY KEY (venueid);
