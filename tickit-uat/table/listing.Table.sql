d4654fa6b6727bc9eeba19d9a520167c0ff6f046
Purgo AI <support@purgo.ai>
2024-09-05 10:27:40 -0500

    Commit message

--- tickit-uat/table/listing.Table.sql
+++ tickit-uat/table/listing.Table.sql
@@ -1,12 +1,13 @@
 
 CREATE TABLE tickit.listing (
-	listid INTEGER NOT NULL, 
-	sellerid INTEGER NOT NULL, 
-	eventid INTEGER NOT NULL, 
+	listid INT NOT NULL, 
+	sellerid INT NOT NULL, 
+	eventid INT NOT NULL, 
 	dateid SMALLINT NOT NULL, 
 	numtickets SMALLINT NOT NULL, 
-	priceperticket NUMERIC(8, 2), 
-	totalprice NUMERIC(8, 2), 
-	listtime TIMESTAMP WITHOUT TIME ZONE
-) DISTSTYLE KEY DISTKEY (listid) SORTKEY (dateid)
+	priceperticket DECIMAL(8, 2), 
+	totalprice DECIMAL(8, 2), 
+	listtime TIMESTAMP
+) USING DELTA;
 
+ALTER TABLE tickit.listing ADD CONSTRAINT pk_listid PRIMARY KEY (listid);
