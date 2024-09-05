91ee91656037ce70463b40a21df499c04f42c1c7
Purgo AI <support@purgo.ai>
2024-09-05 10:27:42 -0500

    Commit message

--- tickit-uat/table/sales.Table.sql
+++ tickit-uat/table/sales.Table.sql
@@ -1,14 +1,15 @@
-
 CREATE TABLE tickit.sales (
-	salesid INTEGER NOT NULL, 
-	listid INTEGER NOT NULL, 
-	sellerid INTEGER NOT NULL, 
-	buyerid INTEGER NOT NULL, 
-	eventid INTEGER NOT NULL, 
-	dateid SMALLINT NOT NULL, 
-	qtysold SMALLINT NOT NULL, 
-	pricepaid NUMERIC(8, 2), 
-	commission NUMERIC(8, 2), 
-	saletime TIMESTAMP WITHOUT TIME ZONE
-) DISTSTYLE KEY DISTKEY (listid) SORTKEY (dateid)
-
+    salesid INT NOT NULL, 
+    listid INT NOT NULL, 
+    sellerid INT NOT NULL, 
+    buyerid INT NOT NULL, 
+    eventid INT NOT NULL, 
+    dateid SMALLINT NOT NULL, 
+    qtysold SMALLINT NOT NULL, 
+    pricepaid DECIMAL(8, 2), 
+    commission DECIMAL(8, 2), 
+    saletime TIMESTAMP
+)
+USING DELTA
+PARTITIONED BY (listid)
+CLUSTERED BY (dateid) INTO 32 BUCKETS
