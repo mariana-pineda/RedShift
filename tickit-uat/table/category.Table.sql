ebf08d2b1c3c2c867cae7bfb42ce07ab174249ce
Purgo AI <support@purgo.ai>
2024-09-05 10:27:33 -0500

    Commit message

--- tickit-uat/table/category.Table.sql
+++ tickit-uat/table/category.Table.sql
@@ -1,8 +1,9 @@
 
 CREATE TABLE tickit.category (
-	catid SMALLINT NOT NULL, 
-	catgroup VARCHAR(10), 
-	catname VARCHAR(10), 
-	catdesc VARCHAR(50)
-) DISTSTYLE KEY DISTKEY (catid) SORTKEY (catid)
-
+    catid SMALLINT NOT NULL, 
+    catgroup STRING, 
+    catname STRING, 
+    catdesc STRING,
+    PRIMARY KEY (catid)
+) USING DELTA
+PARTITIONED BY (catid)
