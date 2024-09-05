2d5ed6e94618953ef7c8172eeaff68ad7ff01ba7
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-09-05 10:28:29 -0500

    Delete tickit-uat/table directory

--- tickit-uat/table/event.Table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE tickit.event (
-    eventid INTEGER NOT NULL PRIMARY KEY, 
-    venueid SMALLINT NOT NULL, 
-    catid SMALLINT NOT NULL, 
-    dateid SMALLINT NOT NULL, 
-    eventname STRING, 
-    starttime TIMESTAMP
-) USING DELTA
--- tickit-uat/table/venue.Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tickit.venue (
-	venueid SMALLINT NOT NULL, 
-	venuename VARCHAR(100), 
-	venuecity VARCHAR(30), 
-	venuestate CHAR(2), 
-	venueseats INTEGER
-) USING DELTA
-PRIMARY KEY (venueid);
