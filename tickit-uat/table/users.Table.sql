28a66ae33ad7a1064efcd5b1220dfbbc4e460602
Purgo AI <support@purgo.ai>
2024-09-05 10:27:45 -0500

    Commit message

--- tickit-uat/table/users.Table.sql
+++ tickit-uat/table/users.Table.sql
@@ -1,22 +1,21 @@
 
 CREATE TABLE tickit.users (
-	userid INTEGER NOT NULL, 
-	username CHAR(8), 
-	firstname VARCHAR(30), 
-	lastname VARCHAR(30), 
-	city VARCHAR(30), 
-	state CHAR(2), 
-	email VARCHAR(100), 
-	phone CHAR(14), 
-	likesports BOOLEAN, 
-	liketheatre BOOLEAN, 
-	likeconcerts BOOLEAN, 
-	likejazz BOOLEAN, 
-	likeclassical BOOLEAN, 
-	likeopera BOOLEAN, 
-	likerock BOOLEAN, 
-	likevegas BOOLEAN, 
-	likebroadway BOOLEAN, 
-	likemusicals BOOLEAN
-) DISTSTYLE KEY DISTKEY (userid) SORTKEY (userid)
-
+    userid INT NOT NULL PRIMARY KEY, 
+    username STRING, 
+    firstname STRING, 
+    lastname STRING, 
+    city STRING, 
+    state STRING, 
+    email STRING, 
+    phone STRING, 
+    likesports BOOLEAN, 
+    liketheatre BOOLEAN, 
+    likeconcerts BOOLEAN, 
+    likejazz BOOLEAN, 
+    likeclassical BOOLEAN, 
+    likeopera BOOLEAN, 
+    likerock BOOLEAN, 
+    likevegas BOOLEAN, 
+    likebroadway BOOLEAN, 
+    likemusicals BOOLEAN
+) USING DELTA
