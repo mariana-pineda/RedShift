fc9b350e4e781a9724f7029a5a1694057fc376a4
Purgo AI <support@purgo.ai>
2024-09-05 10:37:40 -0500

    Commit message

--- tickit-uat/table/event.Table.sql
+++ tickit-uat/table/event.Table.sql
@@ -1,10 +1,13 @@
 
 CREATE TABLE tickit.event (
-	eventid INTEGER NOT NULL, 
-	venueid SMALLINT NOT NULL, 
-	catid SMALLINT NOT NULL, 
-	dateid SMALLINT NOT NULL, 
-	eventname VARCHAR(200), 
-	starttime TIMESTAMP WITHOUT TIME ZONE
-) DISTSTYLE KEY DISTKEY (eventid) SORTKEY (dateid)
+  eventid INTEGER NOT NULL, 
+  venueid SMALLINT NOT NULL, 
+  catid SMALLINT NOT NULL, 
+  dateid SMALLINT NOT NULL, 
+  eventname STRING, 
+  starttime TIMESTAMP
+) USING DELTA
 
+-- Optional: If you need to create indexes, partitions, or other optimizations, you would include them here. 
+-- Example of primary key constraint in Databricks:
+ALTER TABLE tickit.event ADD CONSTRAINT event_pk PRIMARY KEY (eventid);
