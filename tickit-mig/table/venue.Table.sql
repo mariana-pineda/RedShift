6212ecb36232367a377657e7a6bcc5fee0c280ea
Purgo AI <support@purgo.ai>
2024-09-17 13:50:43 -0500

    Commit message

---
+++ tickit-mig/table/venue.Table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE tickit.venue (
+	venueid SMALLINT NOT NULL, 
+	venuename STRING, 
+	venuecity STRING, 
+	venuestate STRING, 
+	venueseats INT,
+	PRIMARY KEY (venueid)
+) USING DELTA
+
