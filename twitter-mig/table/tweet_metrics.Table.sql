ff18115c7a290757bcf1923506e73d503ea4cd3e
Purgo AI <support@purgo.ai>
2024-09-23 12:33:37 -0500

    Commit message

---
+++ twitter-mig/table/tweet_metrics.Table.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE twitter.tweet_metrics (
+  created_at TIMESTAMP, 
+  edit_history_tweet_ids STRING, 
+  followers BIGINT, 
+  following BIGINT, 
+  impression_count BIGINT, 
+  insert_dttm TIMESTAMP, 
+  like_count BIGINT, 
+  quote_count BIGINT, 
+  reply_count BIGINT, 
+  retweet_count BIGINT, 
+  text STRING, 
+  tweet_id STRING, 
+  hashttags STRING
+)
