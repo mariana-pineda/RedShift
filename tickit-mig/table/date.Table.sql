c7bf0ee4ca9072e504b8e697852af560e80db1ab
Purgo AI <support@purgo.ai>
2024-09-23 13:46:07 -0500

    Commit message

--- tickit-mig/table/date.Table.sql
+++ tickit-mig/table/date.Table.sql
@@ -1,17 +1,14 @@
 
 CREATE TABLE tickit.date (
-    dateid SMALLINT NOT NULL,
-    caldate DATE NOT NULL,
-    day STRING NOT NULL,
-    week SMALLINT NOT NULL,
-    month STRING NOT NULL,
-    qtr STRING NOT NULL,
-    year SMALLINT NOT NULL,
+    dateid SMALLINT NOT NULL, 
+    caldate DATE NOT NULL, 
+    day STRING NOT NULL, 
+    week SMALLINT NOT NULL, 
+    month STRING NOT NULL, 
+    qtr STRING NOT NULL, 
+    year SMALLINT NOT NULL, 
     holiday BOOLEAN DEFAULT false
-)
-USING DELTA
+) USING DELTA 
 OPTIONS (
-    PRIMARY KEY (dateid)
+    primaryKey = 'dateid'
 );
-
-COMMENT ON TABLE tickit.date IS 'DISTSTYLE KEY DISTKEY (dateid) SORTKEY (dateid)';
