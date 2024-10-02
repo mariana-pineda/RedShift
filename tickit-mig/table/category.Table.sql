14f72f0070b65db2c1376a26dfe5b0cb5828bbdf
Purgo AI <support@purgo.ai>
2024-10-02 16:57:28 -0500

    Commit message

--- tickit-mig/table/category.Table.sql
+++ tickit-mig/table/category.Table.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE tickit.category (
-  catid SMALLINT NOT NULL, 
-  catgroup STRING, 
-  catname STRING, 
-  catdesc STRING,
-  PRIMARY KEY (catid)
-) USING DELTA
+    catid SMALLINT NOT NULL,
+    catgroup STRING,
+    catname STRING,
+    catdesc STRING,
+    PRIMARY KEY (catid)
+) USING DELTA;
