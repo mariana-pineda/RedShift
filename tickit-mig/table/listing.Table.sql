be64ea2216b52eeabc63526c86014f54e8648af4
Purgo AI <support@purgo.ai>
2024-10-02 16:57:33 -0500

    Commit message

--- tickit-mig/table/listing.Table.sql
+++ tickit-mig/table/listing.Table.sql
@@ -1,13 +1,14 @@
 
 CREATE TABLE tickit.listing (
-  listid INT NOT NULL, 
-  sellerid INT NOT NULL, 
-  eventid INT NOT NULL, 
-  dateid SMALLINT NOT NULL, 
-  numtickets SMALLINT NOT NULL, 
-  priceperticket DECIMAL(8, 2), 
-  totalprice DECIMAL(8, 2), 
-  listtime TIMESTAMP
-)
-USING delta
-OPTIONS (PRIMARY KEY (listid));
+    listid INT NOT NULL, 
+    sellerid INT NOT NULL, 
+    eventid INT NOT NULL, 
+    dateid SMALLINT NOT NULL, 
+    numtickets SMALLINT NOT NULL, 
+    priceperticket DECIMAL(8, 2), 
+    totalprice DECIMAL(8, 2), 
+    listtime TIMESTAMP
+) 
+USING DELTA
+PARTITIONED BY (dateid)
+
