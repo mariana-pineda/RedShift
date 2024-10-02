c4b0bd6472303aabd54fcd9ea944ddcbdc2a352c
Purgo AI <support@purgo.ai>
2024-10-02 16:57:27 -0500

    Commit message

--- tickit-mig/table/venue.Table.sql
+++ tickit-mig/table/venue.Table.sql
@@ -5,7 +5,8 @@ CREATE TABLE tickit.venue (
     venuecity STRING, 
     venuestate STRING, 
     venueseats INT
-);
-
-ALTER TABLE tickit.venue
-ADD CONSTRAINT pk_venue PRIMARY KEY (venueid);
+)
+USING DELTA
+OPTIONS (
+    'primaryKey' = 'venueid'
+)
