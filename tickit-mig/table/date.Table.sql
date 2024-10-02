a617a12fd14c9e5a6a6691a9d47df3e634f6c559
Purgo AI <support@purgo.ai>
2024-10-02 17:04:54 -0500

    Commit message

--- tickit-mig/table/date.Table.sql
+++ tickit-mig/table/date.Table.sql
@@ -2,12 +2,10 @@
 CREATE TABLE tickit.date (
   dateid SMALLINT NOT NULL, 
   caldate DATE NOT NULL, 
-  day STRING NOT NULL, 
+  day STRING(3) NOT NULL, 
   week SMALLINT NOT NULL, 
-  month STRING NOT NULL, 
-  qtr STRING NOT NULL, 
+  month STRING(5) NOT NULL, 
+  qtr STRING(5) NOT NULL, 
   year SMALLINT NOT NULL, 
   holiday BOOLEAN DEFAULT false
-) 
-USING DELTA
-CLUSTERED BY (dateid) INTO 1 BUCKETS
+);
