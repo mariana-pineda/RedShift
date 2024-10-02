df62980b6865ca2eb37a24c6232c8ab875b22cb3
Purgo AI <support@purgo.ai>
2024-10-02 16:57:31 -0500

    Commit message

--- tickit-mig/table/event.Table.sql
+++ tickit-mig/table/event.Table.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE tickit.event (
-  eventid INT NOT NULL,
-  venueid SMALLINT NOT NULL,
-  catid SMALLINT NOT NULL,
-  dateid SMALLINT NOT NULL,
-  eventname STRING,
-  starttime TIMESTAMP
+	eventid INT NOT NULL,
+	venueid SMALLINT NOT NULL,
+	catid SMALLINT NOT NULL,
+	dateid SMALLINT NOT NULL,
+	eventname STRING,
+	starttime TIMESTAMP
 ) USING DELTA
+OPTIONS (primaryKey = 'eventid');
