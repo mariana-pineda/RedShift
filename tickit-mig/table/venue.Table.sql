282c8cdfd07e0af641fa9bbc8f9948891bfdc00a
Purgo AI <support@purgo.ai>
2024-10-03 16:13:38 -0500

    Commit message

--- tickit-mig/table/venue.Table.sql
+++ tickit-mig/table/venue.Table.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE tickit.venue (
-    venueid SMALLINT NOT NULL, 
-    venuename STRING, 
-    venuecity STRING, 
-    venuestate CHAR(2), 
-    venueseats INT
+  venueid SMALLINT NOT NULL, 
+  venuename STRING, 
+  venuecity STRING, 
+  venuestate STRING, 
+  venueseats INT,
+  PRIMARY KEY (venueid)
 ) USING DELTA
-TBLPROPERTIES ('delta.autoOptimize.optimizeWrite' = 'true', 'delta.autoOptimize.autoCompact' = 'true');
+
