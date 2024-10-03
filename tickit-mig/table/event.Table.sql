31a69d791aa55aa16d0ba2e16118e515691e7133
Purgo AI <support@purgo.ai>
2024-10-03 13:33:19 -0500

    Commit message

--- tickit-mig/table/event.Table.sql
+++ tickit-mig/table/event.Table.sql
@@ -1,12 +1,13 @@
 
 CREATE TABLE tickit.event (
-    eventid INT NOT NULL, 
-    venueid SMALLINT NOT NULL, 
-    catid SMALLINT NOT NULL, 
-    dateid SMALLINT NOT NULL, 
-    eventname STRING, 
-    starttime TIMESTAMP
-) USING DELTA
+  eventid INT NOT NULL, 
+  venueid SMALLINT NOT NULL, 
+  catid SMALLINT NOT NULL, 
+  dateid SMALLINT NOT NULL, 
+  eventname STRING, 
+  starttime TIMESTAMP
+)
+USING DELTA
 OPTIONS (
-    primaryKey = 'eventid'
-);
+  COMMENT = 'Databricks Delta table converted from SQL Server'
+)
