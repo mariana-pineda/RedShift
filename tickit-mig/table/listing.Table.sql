e6f630c05795360a1cbe0443c7d6ff1988fb86a4
Purgo AI <support@purgo.ai>
2024-10-03 13:33:21 -0500

    Commit message

--- tickit-mig/table/listing.Table.sql
+++ tickit-mig/table/listing.Table.sql
@@ -1,12 +1,14 @@
 
 CREATE TABLE tickit.listing (
-  listid INT NOT NULL, 
-  sellerid INT NOT NULL, 
-  eventid INT NOT NULL, 
-  dateid SMALLINT NOT NULL, 
-  numtickets SMALLINT NOT NULL, 
-  priceperticket DECIMAL(8, 2), 
-  totalprice DECIMAL(8, 2), 
-  listtime TIMESTAMP
+    listid INT NOT NULL, 
+    sellerid INT NOT NULL, 
+    eventid INT NOT NULL, 
+    dateid SMALLINT NOT NULL, 
+    numtickets SMALLINT NOT NULL, 
+    priceperticket DECIMAL(8, 2), 
+    totalprice DECIMAL(8, 2), 
+    listtime TIMESTAMP
 ) USING DELTA
-LOCATION '/mnt/delta/tickit_listing'
+OPTIONS (
+    partitioned_by = ARRAY('dateid')
+);
