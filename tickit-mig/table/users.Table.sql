59bd93131d1e513258cb533724535ebf2b2ecf2b
Purgo AI <support@purgo.ai>
2024-10-04 10:45:47 -0500

    Commit message

--- tickit-mig/table/users.Table.sql
+++ tickit-mig/table/users.Table.sql
@@ -1,23 +1,24 @@
 
 CREATE TABLE tickit.users (
-    userid INT NOT NULL, 
-    username STRING, 
-    firstname STRING, 
-    lastname STRING, 
-    city STRING, 
-    state STRING, 
-    email STRING, 
-    phone STRING, 
-    likesports BOOLEAN, 
-    liketheatre BOOLEAN, 
-    likeconcerts BOOLEAN, 
-    likejazz BOOLEAN, 
-    likeclassical BOOLEAN, 
-    likeopera BOOLEAN, 
-    likerock BOOLEAN, 
-    likevegas BOOLEAN, 
-    likebroadway BOOLEAN, 
-    likemusicals BOOLEAN
-) 
-USING delta
-PARTITIONED BY (userid)
+  userid INT NOT NULL,
+  username STRING,
+  firstname STRING,
+  lastname STRING,
+  city STRING,
+  state STRING,
+  email STRING,
+  phone STRING,
+  likesports BOOLEAN,
+  liketheatre BOOLEAN,
+  likeconcerts BOOLEAN,
+  likejazz BOOLEAN,
+  likeclassical BOOLEAN,
+  likeopera BOOLEAN,
+  likerock BOOLEAN,
+  likevegas BOOLEAN,
+  likebroadway BOOLEAN,
+  likemusicals BOOLEAN
+)
+USING DELTA
+-- Removing DISTRIB and SORTKEY as Databricks handles distribution and sorting internally
+
