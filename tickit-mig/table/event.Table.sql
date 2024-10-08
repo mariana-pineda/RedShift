8b9ad9a881b7e3b166624d360a9a2b1e23a2e80f
Purgo AI <support@purgo.ai>
2024-10-08 09:30:00 -0500

    Commit message

--- tickit-mig/table/event.Table.sql
+++ tickit-mig/table/event.Table.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE tickit.event (
-  eventid INT NOT NULL,
-  venueid SMALLINT NOT NULL,
-  catid SMALLINT NOT NULL,
-  dateid SMALLINT NOT NULL,
-  eventname STRING,
-  starttime TIMESTAMP
-);
-
-ALTER TABLE tickit.event ADD CONSTRAINT pk_event PRIMARY KEY (eventid);
+    eventid INT NOT NULL,
+    venueid SMALLINT NOT NULL,
+    catid SMALLINT NOT NULL,
+    dateid SMALLINT NOT NULL,
+    eventname STRING,
+    starttime TIMESTAMP
+)
+USING DELTA
+CLUSTERED BY (eventid) SORTED BY (dateid)
