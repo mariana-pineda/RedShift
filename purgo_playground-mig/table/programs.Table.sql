5c43ecc9736490f0a156b7a8a4c6cfe2042edef8
Purgo AI <support@purgo.ai>
2024-10-21 18:27:46 -0500

    Commit message

--- purgo_playground-mig/table/programs.Table.sql
+++ purgo_playground-mig/table/programs.Table.sql
@@ -5,4 +5,7 @@ CREATE TABLE purgo_playground.programs (
   country_code STRING NOT NULL, 
   program_start_date TIMESTAMP
 )
-USING DELTA;
+USING DELTA
+OPTIONS (
+  path = 'dbfs:/path/to/purgo_playground/programs'
+);
