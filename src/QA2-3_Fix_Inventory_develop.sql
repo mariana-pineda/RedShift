985b7db01c90fac9b87eb04af356f4346066c401
Purgo AI <support@purgo.ai>
2024-11-01 09:47:40 -0500

    Commit message

--- src/QA2-3_Fix_Inventory_develop.sql
+++ src/QA2-3_Fix_Inventory_develop.sql
@@ -1,20 +1,5 @@
 ALTER TABLE qa.employees
-ADD COLUMN lastdate DATE DEFAULT CURRENT_DATE;
+ADD COLUMN lastdate TIMESTAMP;
 
-ALTER TABLE qa.customers
-ADD COLUMN categoryGroup VARCHAR DEFAULT 'Uncategorized';
-
-CREATE OR REPLACE FUNCTION validate_categorygroup() RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.categoryGroup NOT IN ('VIP', 'Regular', 'New', 'Uncategorized') THEN
-        RAISE EXCEPTION 'Invalid categoryGroup %', NEW.categoryGroup;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS validate_categorygroup_trigger ON qa.customers;
-
-CREATE TRIGGER validate_categorygroup_trigger
-BEFORE INSERT OR UPDATE ON qa.customers
-FOR EACH ROW EXECUTE FUNCTION validate_categorygroup();
+ALTER TABLE qa_1.customers
+ADD COLUMN categoryGroup STRING;
