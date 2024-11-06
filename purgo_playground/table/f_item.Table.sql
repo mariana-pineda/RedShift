7887f4581047b9d8621ea0d955748e5eeee232c5
Purgo AI <support@purgo.ai>
2024-11-06 18:10:53 -0500

    Commit message

--- purgo_playground/table/f_item.Table.sql
+++ purgo_playground/table/f_item.Table.sql
@@ -1,16 +1,15 @@
 
 CREATE TABLE purgo_playground.f_item (
-	item_nbr VARCHAR(256), 
-	item_vb BIGINT, 
-	delivery_dt NUMERIC(38, 0), 
-	item_inv_type VARCHAR(256), 
-	txn_nbr VARCHAR(256), 
-	primary_qty DOUBLE PRECISION, 
-	qty_in_hand DOUBLE PRECISION, 
-	qty_on_hold DOUBLE PRECISION, 
-	item_serial_nbr VARCHAR(256), 
-	package_nbr VARCHAR(256), 
-	crt_dt TIMESTAMP WITHOUT TIME ZONE, 
-	updt_dt TIMESTAMP WITHOUT TIME ZONE
+  item_nbr STRING, 
+  item_vb LONG, 
+  delivery_dt DECIMAL(38, 0), 
+  item_inv_type STRING, 
+  txn_nbr STRING, 
+  primary_qty DOUBLE, 
+  qty_in_hand DOUBLE, 
+  qty_on_hold DOUBLE, 
+  item_serial_nbr STRING, 
+  package_nbr STRING, 
+  crt_dt TIMESTAMP, 
+  updt_dt TIMESTAMP
 )
-
