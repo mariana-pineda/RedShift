b5a15ea30a4adb3e4f377197cf55acc93f79b752
Purgo AI <support@purgo.ai>
2024-11-07 11:02:09 -0500

    Commit message

--- purgo_playground/table/event_log.Table.sql
+++ purgo_playground/table/event_log.Table.sql
@@ -1,17 +1,18 @@
 
 CREATE TABLE purgo_playground.event_log (
-  eventid STRING,
-  version STRING,
-  task STRING,
-  systemtime STRING,
-  eventrecordid STRING,
-  subjectusername STRING,
-  subjectdomainname STRING,
-  objecttype STRING,
-  ipaddress STRING,
-  ipport STRING,
-  sharename STRING,
-  sharelocalpath STRING,
-  accesslist STRING,
-  objectname STRING
+	eventid VARCHAR(256), 
+	version VARCHAR(256), 
+	task VARCHAR(256), 
+	systemtime VARCHAR(256), 
+	eventrecordid VARCHAR(256), 
+	subjectusername VARCHAR(256), 
+	subjectdomainname VARCHAR(256), 
+	objecttype VARCHAR(256), 
+	ipaddress VARCHAR(256), 
+	ipport VARCHAR(256), 
+	sharename VARCHAR(256), 
+	sharelocalpath VARCHAR(256), 
+	accesslist VARCHAR(256), 
+	objectname VARCHAR(256)
 )
+
