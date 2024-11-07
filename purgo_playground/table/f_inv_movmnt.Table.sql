8fd1ff011ab830e3e155502d6717cd1a64d5e985
Purgo AI <support@purgo.ai>
2024-11-07 11:01:58 -0500

    Commit message

--- purgo_playground/table/f_inv_movmnt.Table.sql
+++ purgo_playground/table/f_inv_movmnt.Table.sql
@@ -1,21 +1,21 @@
 
 CREATE TABLE purgo_playground.f_inv_movmnt (
-    txn_id STRING, 
-    inv_loc STRING, 
-    financial_qty DOUBLE, 
-    net_qty DOUBLE, 
-    expired_qt DECIMAL(38, 0), 
-    item_nbr STRING, 
-    unit_cost DOUBLE, 
-    um_rate DOUBLE, 
-    plant_loc_cd STRING, 
-    inv_stock_reference STRING, 
-    stock_type STRING, 
-    qty_on_hand DOUBLE, 
-    qty_shipped DOUBLE, 
-    cancel_dt DECIMAL(38, 0), 
-    flag_active STRING, 
-    crt_dt TIMESTAMP, 
-    updt_dt TIMESTAMP
+	txn_id VARCHAR(256), 
+	inv_loc VARCHAR(256), 
+	financial_qty DOUBLE PRECISION, 
+	net_qty DOUBLE PRECISION, 
+	expired_qt NUMERIC(38, 0), 
+	item_nbr VARCHAR(256), 
+	unit_cost DOUBLE PRECISION, 
+	um_rate DOUBLE PRECISION, 
+	plant_loc_cd VARCHAR(256), 
+	inv_stock_reference VARCHAR(256), 
+	stock_type VARCHAR(256), 
+	qty_on_hand DOUBLE PRECISION, 
+	qty_shipped DOUBLE PRECISION, 
+	cancel_dt NUMERIC(38, 0), 
+	flag_active VARCHAR(10), 
+	crt_dt TIMESTAMP WITHOUT TIME ZONE, 
+	updt_dt TIMESTAMP WITHOUT TIME ZONE
 )
 
