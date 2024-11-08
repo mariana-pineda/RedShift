d66af1114f631d90c6b3e5142dc00235fe957b3b
Purgo AI <support@purgo.ai>
2024-11-08 18:06:32 -0500

    Commit message

--- src/QA1-2_Fix_Inventory_develop.sql
+++ src/QA1-2_Fix_Inventory_develop.sql
@@ -1,9 +1,10 @@
--- SQL code to add columns to the respective tables
+-- SQL script to update the database schema according to the user story
 
 -- Add "lastdate" column to "employees" table
 ALTER TABLE qa.employees
-ADD COLUMN lastdate TIMESTAMP;
+ADD COLUMN lastdate DATE DEFAULT CURRENT_DATE;
 
 -- Add "categoryGroup" column to "customers" table
 ALTER TABLE qa.customers
-ADD COLUMN categoryGroup VARCHAR(255);
+ADD COLUMN categoryGroup VARCHAR(50);
+
