b1f6f1f1fe75691269f5ab6661779ec5888f8343
Purgo AI <support@purgo.ai>
2024-11-08 17:45:45 -0500

    Commit message

--- purgo_playground-mig/table/programs.Table.sql
+++ purgo_playground-mig/table/programs.Table.sql
@@ -4,4 +4,6 @@ CREATE TABLE purgo_playground.programs (
     program_name STRING NOT NULL, 
     country_code STRING NOT NULL, 
     program_start_date TIMESTAMP
-)
+);
+
+ALTER TABLE purgo_playground.programs ADD CONSTRAINT programs_pk PRIMARY KEY (program_id);
