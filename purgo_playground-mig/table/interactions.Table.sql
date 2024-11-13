61f88b98b08b0188f118bbfe69710223d8d09fff
Purgo AI <support@purgo.ai>
2024-11-13 16:41:15 -0500

    Commit message

--- purgo_playground-mig/table/interactions.Table.sql
+++ purgo_playground-mig/table/interactions.Table.sql
@@ -1,9 +1,8 @@
 
 CREATE TABLE purgo_playground.interactions (
-	interaction_id INT NOT NULL, 
-	enrollment_id INT NOT NULL, 
-	interaction_date TIMESTAMP
+  interaction_id INT NOT NULL, 
+  enrollment_id INT NOT NULL, 
+  interaction_date TIMESTAMP
 );
 
--- Databricks SQL does not support SQL Server syntax for specifying primary keys during table creation,
--- so ensure primary key constraints are managed within the application or logical model.
+ALTER TABLE purgo_playground.interactions ADD CONSTRAINT pk_interactions PRIMARY KEY (interaction_id);
