6e5de7d9f83f98e677d78bacd08f5417ff4aa06c
Purgo AI <support@purgo.ai>
2024-11-13 16:50:25 -0500

    Commit message

--- dbo-mig/view/Invoices.View.sql
+++ dbo-mig/view/Invoices.View.sql
@@ -1,41 +1,43 @@
 
-CREATE OR REPLACE VIEW Invoices AS
+CREATE VIEW Invoices AS
 SELECT 
-    Orders.ShipName, 
-    Orders.ShipAddress, 
-    Orders.ShipCity, 
-    Orders.ShipRegion, 
-    Orders.ShipPostalCode, 
-    Orders.ShipCountry, 
-    Orders.CustomerID, 
-    Customers.CompanyName AS CustomerName, 
-    Customers.Address, 
-    Customers.City, 
-    Customers.Region, 
-    Customers.PostalCode, 
-    Customers.Country, 
-    (Employees.FirstName || ' ' || Employees.LastName) AS Salesperson, 
-    Orders.OrderID, 
-    Orders.OrderDate, 
-    Orders.RequiredDate, 
-    Orders.ShippedDate, 
-    Shippers.CompanyName AS ShipperName, 
-    Order_Details.ProductID, 
-    Products.ProductName, 
-    Order_Details.UnitPrice, 
-    Order_Details.Quantity, 
-    Order_Details.Discount, 
-    ((Order_Details.UnitPrice * Order_Details.Quantity * (1 - Order_Details.Discount)) * 100.0 / 100.0) AS ExtendedPrice, 
-    Orders.Freight
+  Orders.ShipName, 
+  Orders.ShipAddress, 
+  Orders.ShipCity, 
+  Orders.ShipRegion, 
+  Orders.ShipPostalCode, 
+  Orders.ShipCountry, 
+  Orders.CustomerID, 
+  Customers.CompanyName AS CustomerName, 
+  Customers.Address, 
+  Customers.City, 
+  Customers.Region, 
+  Customers.PostalCode, 
+  Customers.Country, 
+  CONCAT(FirstName, ' ', LastName) AS Salesperson, 
+  Orders.OrderID, 
+  Orders.OrderDate, 
+  Orders.RequiredDate, 
+  Orders.ShippedDate, 
+  Shippers.CompanyName AS ShipperName, 
+  `Order Details`.ProductID, 
+  Products.ProductName, 
+  `Order Details`.UnitPrice, 
+  `Order Details`.Quantity, 
+  `Order Details`.Discount, 
+  (ROUND(`Order Details`.UnitPrice * `Order Details`.Quantity * (1 - `Order Details`.Discount), 2)) AS ExtendedPrice, 
+  Orders.Freight
 FROM 
-    Shippers 
-JOIN 
-    Orders ON Shippers.ShipperID = Orders.ShipVia
-JOIN 
-    Customers ON Customers.CustomerID = Orders.CustomerID
-JOIN 
-    Employees ON Employees.EmployeeID = Orders.EmployeeID
-JOIN 
-    Order_Details ON Orders.OrderID = Order_Details.OrderID
-JOIN 
-    Products ON Products.ProductID = Order_Details.ProductID;
+  Shippers 
+  INNER JOIN (
+    Products 
+    INNER JOIN (
+      Employees 
+      INNER JOIN (
+        Customers 
+        INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+      ) ON Employees.EmployeeID = Orders.EmployeeID
+      INNER JOIN `Order Details` ON Orders.OrderID = `Order Details`.OrderID
+    ) ON Products.ProductID = `Order Details`.ProductID
+  ) ON Shippers.ShipperID = Orders.ShipVia
+
