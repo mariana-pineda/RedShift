5537821e1fff752bf128c6838fd0f431b6146370
Purgo AI <support@purgo.ai>
2024-11-14 11:02:14 -0500

    Commit message

--- purgo_playground-mig/table/interactions.Table.sql
+++ purgo_playground-mig/table/interactions.Table.sql
@@ -3,6 +3,5 @@ CREATE TABLE purgo_playground.interactions (
   interaction_id INT NOT NULL, 
   enrollment_id INT NOT NULL, 
   interaction_date TIMESTAMP
-);
+)
 
-ALTER TABLE purgo_playground.interactions ADD CONSTRAINT pk_interactions PRIMARY KEY (interaction_id);
