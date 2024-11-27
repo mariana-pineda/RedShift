44fc3400efe4265bb5c06604cfb47a082e4c60bf
Purgo AI <support@purgo.ai>
2024-11-27 10:15:18 -0500

    Commit message

--- purgo_playground-mig/table/interactions.Table.sql
+++ purgo_playground-mig/table/interactions.Table.sql
@@ -1,7 +1,6 @@
 
 CREATE TABLE purgo_playground.interactions (
-    interaction_id INT NOT NULL, 
-    enrollment_id INT NOT NULL, 
-    interaction_date TIMESTAMP 
-)
-USING DELTA;
+  interaction_id INT NOT NULL, 
+  enrollment_id INT NOT NULL, 
+  interaction_date TIMESTAMP
+);
