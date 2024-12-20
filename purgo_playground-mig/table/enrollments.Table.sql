a521d42fa6e69e7dfc3bf30a78795209505eacdd
Purgo AI <support@purgo.ai>
2024-12-20 09:38:36 -0500

    Commit message

--- purgo_playground-mig/table/enrollments.Table.sql
+++ purgo_playground-mig/table/enrollments.Table.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE purgo_playground.enrollments (
-  enrollment_id INT NOT NULL,
-  program_id INT NOT NULL,
-  enrollment_date TIMESTAMP
+  enrollment_id INT NOT NULL COMMENT 'This is the primary identifier for enrollments', 
+  program_id INT NOT NULL COMMENT 'This is the identifier for programs', 
+  enrollment_date TIMESTAMP COMMENT 'This is the date of enrollment'
 )
 USING DELTA;
